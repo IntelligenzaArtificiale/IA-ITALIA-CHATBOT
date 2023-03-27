4b4c58ef5e187f88424c5d9af8e068660f86cbef
IntelligenzaArtificiale <108482353+IntelligenzaArtificiale@users.noreply.github.com>
2023-03-27 21:57:28 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,24 +1,44 @@
+
 import streamlit as st
 
 """
 ## Web scraping on Streamlit Cloud with Selenium
 """
 
-with st.echo():
-    from selenium import webdriver
-    from selenium.webdriver.chrome.options import Options
-    from selenium.webdriver.chrome.service import Service
-    from webdriver_manager.chrome import ChromeDriverManager
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
 
-    @st.experimental_singleton
-    def get_driver():
-        return webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+@st.experimental_singleton
+def get_driver():
+    return webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
 
-    options = Options()
-    options.add_argument('--disable-gpu')
-    options.add_argument('--headless')
+options = Options()
+options.add_argument('--disable-gpu')
+options.add_argument('--headless')
 
-    driver = get_driver()
-    driver.get("http://example.com")
+driver = get_driver()
 
-    st.code(driver.page_source)
+# open https://deepai.org/machine-learning-model/text-generator
+# find textarea with class "model-input-text-input"
+# send prompt variable to textarea
+# click button with id = "modelSubmitButton"
+# read all text int div with class "try-it-result-area"
+
+prompt = st.text_input("Prompt", "The quick brown fox jumps over the lazy dog.")
+if st.button("Generate"):
+  with st.spinner("Generating..."):
+    driver.get("https://deepai.org/machine-learning-model/text-generator")
+    textarea = driver.find_element_by_class_name("model-input-text-input")
+    textarea.send_keys(prompt)
+    button = driver.find_element_by_id("modelSubmitButton")
+    button.click()
+    # wait for result
+    #ogni 3 secondi controlla se il risultato è pronto
+    reuslt = ""
+    while result == "":
+        result = driver.find_element_by_class_name("try-it-result-area").text
+        time.sleep(3)
+    st.code(result)
+  
