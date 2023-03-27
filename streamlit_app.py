6b934008a2a13c8501f96eef3ed8b381b70260e0
IntelligenzaArtificiale <108482353+IntelligenzaArtificiale@users.noreply.github.com>
2023-03-27 22:18:25 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,16 +26,20 @@ prompt = st.text_input("Prompt", "The quick brown fox jumps over the lazy dog.")
 if st.button("Generate"):
   with st.spinner("Generating..."):
     driver.get("https://deepai.org/machine-learning-model/text-generator")
-    st.code(driver.page_source)
-
+    print(driver.page_source)
+    time.sleep(5)
 
     textarea = driver.find_element(By.CLASS_NAME, "model-input-text-input")
     textarea.send_keys(prompt)
+    time.sleep(5)
+    print(textarea.text)
     
     button = driver.find_element(By.ID, "modelSubmitButton")
+    time.sleep(5)
     button.click()
     # wait for result
     #ogni 3 secondi controlla se il risultato è pronto
+    time.sleep(5)
     reuslt = ""
     while result == "":
         result = driver.find_element(By.CLASS_NAME, "try-it-result-area").text
