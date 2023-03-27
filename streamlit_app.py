d04ebcac693919634f55bfa40fe2283107d752d2
IntelligenzaArtificiale <108482353+IntelligenzaArtificiale@users.noreply.github.com>
2023-03-27 22:22:46 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,6 +11,8 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
+import time 
+
 
 @st.experimental_singleton
 def get_driver():
@@ -23,6 +25,7 @@ options.add_argument('--headless')
 driver = get_driver()
 
 prompt = st.text_input("Prompt", "The quick brown fox jumps over the lazy dog.")
+
 if st.button("Generate"):
   with st.spinner("Generating..."):
     driver.get("https://deepai.org/machine-learning-model/text-generator")
