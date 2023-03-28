4ece588f1aa5b89b102c9092031032a813fc0f81
IntelligenzaArtificiale <108482353+IntelligenzaArtificiale@users.noreply.github.com>
2023-03-28 19:35:37 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -122,12 +122,13 @@ if col2.button("Chiedi 🚀") and prompt != "" and driver.page_source != "":
             prompt = prompt[4:]
             new_request = tts.google(prompt, from_language="it", to_language="en")
             image_files = Generate(new_request)
-            html_message_image = f''
+            html_message_image = f'<div style="max-width: 300px; display: flex; flex-wrap: wrap;">'
             if image_files != "Error":
                 for i in image_files:
-                    html_image = f'<img src="{i}" style="max-width: 200px;" /><br>'
+                    html_image = f'<img src="{i}" style="max-width: 100%;  flex-basis: 33.33%;" />'
                     html_message_image += html_image
-                add_message(html_image, 'bot')
+                html_message_image += "</div>"
+                add_message(html_message_image, 'bot')
                 
             else:
                 add_message("🤖 Ops, qualcosa è andato storto, riprova più tardi", 'bot')
