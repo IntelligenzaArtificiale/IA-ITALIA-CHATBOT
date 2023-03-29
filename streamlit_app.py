481782309d1ee8eab6232f191b6c280bbf1c8015
IntelligenzaArtificiale <108482353+IntelligenzaArtificiale@users.noreply.github.com>
2023-03-29 11:51:26 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -62,11 +62,7 @@ def get_driver():
     options.add_argument("--disable-features=VizDisplayCompositor")    
     options.add_argument("'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.182 Safari/537.36'")
     driver = webdriver.Chrome(ChromeDriverManager().install(), options=options)
-
-    driver.get("https://example.com")
-    driver.execute_script("window.open('');")
     driver.get('https://deepai.org/machine-learning-model/text-generator')
-    driver.switch_to.window(driver.window_handles[1])
     st.write(driver.window_handles)
   return driver
 
